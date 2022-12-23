3abeb3fe999fc83a99802ca994513d09e921f62f
OWEN LU <38624816+lujiaweigood@users.noreply.github.com>
2022-12-22 19:21:05 -0500

    add
    add

---
+++ Assignment8Microservices/cs548db/cs548db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+psql -v ON_ERROR_STOP=1 -v password="$DATABASE_PASSWORD" \
+     --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE USER cs548user WITH PASSWORD :'password';
+    CREATE DATABASE cs548 WITH OWNER cs548user;
+    GRANT ALL PRIVILEGES ON DATABASE cs548 TO cs548user;
+EOSQL
